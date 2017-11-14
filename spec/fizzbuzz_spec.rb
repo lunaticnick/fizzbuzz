0417861989cfdde1f6cf964546ab57b2821bb8a6
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-14 13:09:55 +0000

    Finishing Fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,13 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns 1 when passed number' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+  it 'returns 104 when passed number' do
+    expect(fizzbuzz(104)).to eq 104
+  end
 end
